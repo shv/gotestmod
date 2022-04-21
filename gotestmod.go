a560b93dedba36b4c61e68182585ca79340781fe
Aleksey Sharypov <aleksey.sharypov@vkpay.com>
2022-04-21 15:28:52 +0300

    Change Hi to allow multilang

--- gotestmod.go
+++ gotestmod.go
@@ -1,9 +1,24 @@
 package gotestmod
 
-import "fmt" 
+import (
+   "errors"
+   "fmt"
+)
 
 // Hi returns a friendly greeting
-func Hi(name string) string {
+func Hi(name, lang string) (string, error) {
+   switch lang {
+   case en:
+      return fmt.Sprintf("Hi, $s!", name), nil
+   case "pt":
+      return fmt.Sprintf("Oi, %s!", name), nil
+   case "es":
+      return fmt.Sprintf("iHolla, %s!", name), nil
+   case "fr":
+      return fmt.Sprintf("Bonjourm %s!", name), nil
+   default:
+      return "", errors.New("unknown language")
+   }
    return fmt.Sprintf("Hi, %s!", name)
 }
 
